6c33a616d6bb5020fdcf9d9cbcab2a3f5fec0c40
Nico Cid <nicocidips@gmail.com>
2025-11-19 12:33:40 -0300

    Agregar prompt interactivo al cliente

--- cliente.go
+++ cliente.go
@@ -3,10 +3,41 @@ package main
 import (
 	"fmt"
 	"net"
+	"bufio"
+	"os"
+	"strings"
 )
 
+var nameNodeSocket = "192.168.100.96:9000"
+
 func main() {
-	conn, err := net.Dial("tcp", "192.168.100.96:9000")
+	reader := bufio.NewReader(os.Stdin)
+	
+	for {
+		fmt.Print("> ")
+		
+		input, _ := reader.ReadString('\n')
+		command := strings.TrimSpace(input)
+		
+		if command == "exit" {
+			break
+		}
+		
+		fmt.Println("Ejecutaste:", command)
+	}
+
+
+	conn, err := net.Dial("tcp", nameNodeSocket)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Conectado al servidor!")
+}
+
+func ls() {
+	conn, err := net.Dial("tcp", nameNodeSocket)
 	if err != nil {
 		panic(err)
 	}
